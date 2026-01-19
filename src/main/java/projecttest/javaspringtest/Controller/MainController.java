5cd60682b5a94dff8adc14bf909f21b96b2b38df
wonehgus <wonehgus1234@naver.com>
2026-01-19 23:00:49 +0900

    kafka

--- src/main/java/projecttest/javaspringtest/Controller/MainController.java
+++ src/main/java/projecttest/javaspringtest/Controller/MainController.java
@@ -1,13 +1,28 @@
 package projecttest.javaspringtest.Controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import projecttest.javaspringtest.Kafka.KafkaConsumerService;
+import projecttest.javaspringtest.Kafka.KafkaProducerService;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    final private KafkaProducerService kafkaProducerService;
+
     @GetMapping("/")
     public String index(){
         return "index";
     }
+
+    @GetMapping("/kafka")
+    public String kafka(){
+        kafkaProducerService.sendMessage("Kafka 메시지 전송");
+        return "kafka";
+    }
+
+    
+
 
 }

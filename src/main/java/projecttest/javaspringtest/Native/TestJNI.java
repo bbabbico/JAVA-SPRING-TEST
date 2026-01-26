b6d60061ae078583e4925e1ee23ae278aa5bc529
wonehgus <wonehgus1234@naver.com>
2026-01-26 09:32:24 +0900

    NATIVE 절대경로 일반화

--- src/main/java/projecttest/javaspringtest/Native/TestJNI.java
+++ src/main/java/projecttest/javaspringtest/Native/TestJNI.java
@@ -1,6 +1,10 @@
 package projecttest.javaspringtest.Native;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestJNI {
+    static File file = new File("Native\\TestJNI.dll"); // 일반적으로 모든 컴퓨터가 JAVA HOME 이 설정안되어있으니 절대경로 사용하기위해 사용
 
     /**
      * 네이티브 라이브러리 로드 (파일명은 OS가 알아서 접두/확장자 붙임)
@@ -9,7 +13,11 @@ public class TestJNI {
      * macOS  : libTestJNI.dylib
      */
     static { //dll 설정 // 원래 이방식 안쓰는데, 절대경로로 쓰려면 System.load 해야됨.
-        System.load("C:\\Users\\SHPS\\Desktop\\자바\\JAVA - SPRING TEST\\Native\\TestJNI.dll");
+        try {
+            System.load(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
